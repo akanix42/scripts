3a713516c9b53e8fe33fd3b6e6e7dc1cd272d11e
Ashley Reid <1229110+nathantreid@users.noreply.github.com>
2020-07-22 23:19:04 -0500

    fix zsh slowness

--- zsh.zsh
+++ zsh.zsh
@@ -9,3 +9,18 @@ function _accept-line() {
     zle .accept-line
 }
 zle -N accept-line _accept-line
+
+### -----------------------------------
+### Fix slowness of pastes with zsh-syntax-highlighting.zsh
+_zsh_pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+
+_zsh_pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+
+zstyle :bracketed-paste-magic paste-init _zsh_pasteinit
+zstyle :bracketed-paste-magic paste-finish _zsh_pastefinish
+### -----------------------------------
